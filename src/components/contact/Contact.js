ae63fedcfcacd14268c0a04db923f72fceffed95
Matt Bowen <mattbowen03@gmail.com>
2022-08-26 09:19:42 -0500

    another try

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,19 +5,6 @@ export default function Contact() {
     <section className='contact' id='contact'>
       <div>
         <h1>Contact</h1>
-        <form name='portfolio-contact-form' netlify method='POST'>
-          <input type='text' name='name' placeholder='Name' />
-          <input type='email' name='email' placeholder='Email' />
-          <textarea
-            name='message'
-            type='text'
-            id=''
-            cols='30'
-            rows='10'
-            placeholder='Enter your message...'
-          />
-          <input type='submit' value='Send' id='submitBtn' />
-        </form>
         <form name='contact' method='post'>
           <input type='hidden' name='form-name' value='contact' />
           <p>
