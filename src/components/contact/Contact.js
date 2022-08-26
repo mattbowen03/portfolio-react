82c18cfa16fcb35d17a47d9e23b3707205090118
Matt Bowen <mattbowen03@gmail.com>
2022-08-26 13:42:40 -0500

    update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,24 +8,20 @@ export default function Contact() {
         <div className='row'>
           <form name='contact' method='post'>
             <input type='hidden' name='form-name' value='contact' />
-            <p>
-              <label>
-                Your Name: <input type='text' name='name' />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type='email' name='email' />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name='message'></textarea>
-              </label>
-            </p>
-            <p>
-              <button type='submit'>Send</button>
-            </p>
+
+            <label>
+              Your Name: <input type='text' name='name' />
+            </label>
+
+            <label>
+              Your Email: <input type='email' name='email' />
+            </label>
+
+            <label>
+              Message: <textarea name='message'></textarea>
+            </label>
+
+            <button type='submit'>Send</button>
           </form>
         </div>
       </div>
